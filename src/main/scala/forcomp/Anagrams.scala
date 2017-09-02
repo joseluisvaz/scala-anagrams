791e6f9a1cdf0bd5839573a69b394513b075c028
joseluisvaz <joseluisv1201@gmail.com>
2017-09-02 08:04:06 -0500

    sentence ocurrances dictionaryocurrances word anagrams implemented

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -42,7 +42,7 @@ object Anagrams {
   }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.reduce(_ concat _))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
@@ -59,10 +59,10 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.groupBy((word: Word) => wordOccurrences(word))
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
--- src/main/scala/testing.sc
+++ src/main/scala/testing.sc
@@ -6,3 +6,8 @@ val A = "hola mi amor"
 "holaohogla".groupBy(c => c.toLower).map( p => (p._1, p._2.length)).toList
   .sortWith(_._1 < _._1)
 
+val B = List("hola", "mi", "amor", "querido")
+
+B.reduce(_ concat _)
+
+Anagrams.dictionaryByOccurrences
